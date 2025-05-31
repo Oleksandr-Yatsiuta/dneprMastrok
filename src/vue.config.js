42b1fef190d970a70c3012802d17fe842723505f
Sashlychok <y.alexandr.32@gmail.com>
2025-05-31 16:19:56 +0300

    add config

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,4 +1,5 @@
-module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/dneprMastrok/' : '/'
-
-  }
+const {defineConfig} = require('@vue/cli-service')
+module.exports = defineConfig({
+	publicPath : "/dneprMastrok/",
+	transpileDependencies: true
+}) 
